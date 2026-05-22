13ebdd72111d0e242a04a1b6ec2b8975e2147a90
Khoa <taotenk0912@gmail.com>
2026-05-22 15:22:58 +0700

    Fix Vercel gateway API routes

---
+++ api/_gateway.js
@@ -0,0 +1,15 @@
+import { createApiHandler } from "../gateway/backend.mjs";
+
+const handleApi = createApiHandler(process.cwd());
+
+export default async function handler(request, response) {
+  const protocol = request.headers["x-forwarded-proto"] ?? "https";
+  const host = request.headers.host ?? "localhost";
+  const url = new URL(request.url ?? "/", `${protocol}://${host}`);
+
+  const handled = await handleApi(request, response, url);
+  if (!handled && !response.writableEnded) {
+    response.statusCode = 404;
+    response.end("API not found");
+  }
+}
